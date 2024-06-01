5436271e1a7bf2b60c0f9f1c2f5008101b739398
hendisantika <hendisantika@yahoo.co.id>
2024-06-01 15:00:58 +0700

    Add TestSQS 🫸🌀✏️📗🐧🐳⬆

---
+++ src/test/kotlin/id/my/hendisantika/springbootsnssqslocalstack/TestSQS.kt
@@ -0,0 +1,35 @@
+package id.my.hendisantika.springbootsnssqslocalstack
+
+import com.amazonaws.services.sqs.AmazonSQSAsync
+import com.amazonaws.services.sqs.model.Message
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.api.TestMethodOrder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-sns-sqs-localstack
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 01/06/24
+ * Time: 15.00
+ * To change this template use File | Settings | File Templates.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+class TestSQS {
+
+    private val queue = UUID.randomUUID().toString()
+
+    @Autowired
+    private lateinit var amazonSQS: AmazonSQSAsync
+
+    private lateinit var queueUrl: String
+
+    private lateinit var message: Message
+}
